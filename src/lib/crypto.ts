811c7b3b04d0bd049637a7ff7fa18060bdf649cc
Ruben D. Garcia <rdgp99@gmail.com>
2025-10-05 08:11:35 -0500

    gracefully handle lack of env

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -25,8 +25,18 @@ async function getCryptoKey(secret: string, offset = 0, usage: KeyUsage[] = ["en
 
 // --- ENCRYPT ---
 export async function encryptPassword(password: string, ttlMinutes = 10) {
-  const secret = import.meta.env.VITE_ENCRYPTION_BASE_SECRET || "fallback-secret";
+  const secret = import.meta.env.VITE_ENCRYPTION_BASE_SECRET;
   const key = await getCryptoKey(secret, 0, ["encrypt"]);
+
+  if (!secret || secret == "") {
+    console.error("no valid secret found")
+    return
+  }
+
+  if (!password || password == "") {
+    console.error("no valid password found")
+    return
+  } 
 
   const iv = crypto.getRandomValues(new Uint8Array(12)); // 96-bit nonce
   const encoder = new TextEncoder();
